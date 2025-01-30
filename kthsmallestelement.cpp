6297804f1007c48a80feb4189d17eb3ae96799aa
MayankGajbhiye2890 <138212088+MayankGajbhiye2890@users.noreply.github.com>
2025-01-30 13:50:55 +0530

    Update kthsmallestelement.cpp

--- kthsmallestelement.cpp
+++ kthsmallestelement.cpp
@@ -19,3 +19,28 @@ public:
         return result;
     }
 };
+
+//using quickselect algo
+int partition(vector<int>&arr, int low, int high){
+        int pivot = arr[low];
+        int i = low+1;
+        for(int j = low+1; j<=high; j++){
+            if(arr[j]< pivot){
+                swap(arr[i], arr[j]);
+                i++;
+            }
+        }
+        swap(arr[low], arr[i-1]);
+        return i-1;
+    }
+    int quickselect(vector<int>&arr, int k, int low, int high){
+        if(low>high) return 0;
+        int pivot_index = partition(arr, low, high);
+        if(pivot_index == k) return arr[pivot_index];
+        else if(k<pivot_index) return quickselect(arr, k, low, pivot_index -1);
+        else return quickselect(arr,k, pivot_index+1, high);
+        //return -1;
+    }
+    int kthSmallest(vector<int> &arr, int k) {
+       return quickselect(arr, k-1, 0, arr.size()-1);
+    }
